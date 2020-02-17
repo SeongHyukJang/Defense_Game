9af3a42b8101fba719a4cc9590bef5168a457270
eliotjang <eliotjang2@gmail.com>
2020-02-18 01:04:33 +0900

    add spawn enemy
    but not working bullet

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Transform target;
+
+    public float speed = 70f;
+    public void Seek(Transform _target)
+    {
+        target = _target;
+    }
+
+    void Update()
+    {
+        if(target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+
+        if(dir.magnitude <= distanceThisFrame)
+        {
+            HitTarget();
+            return;
+        }
+
+        transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+        
+    }
+
+    void HitTarget()
+    {
+        Debug.Log("We hit sth");
+    }
+}
