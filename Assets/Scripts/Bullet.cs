9387d00b052cdc1e24cde392dee9d080860dffa1
eliotjang <eliotjang2@gmail.com>
2020-02-18 18:44:10 +0900

    add Round  & WaveTime & Life & Menu

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,10 +1,13 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
     private Transform target;
 
     public float speed = 70f;
+    public GameObject impactEffect;
     public void Seek(Transform _target)
     {
         target = _target;
@@ -33,6 +36,10 @@ public class Bullet : MonoBehaviour
 
     void HitTarget()
     {
-        Debug.Log("We hit sth");
+        GameObject effectIns = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(effectIns, 2f);
+
+        Destroy(target.gameObject);
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -1,27 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+
 
 public class WaveSpawner : MonoBehaviour
 {
+    GameObject[] Enemy;
+
     public Transform enemyPrefab;
 
     public Transform spawnPoint;
 
-    public float timeBetweenWaves = 10F;
+    public static int enemyCount = 0;
+
+    public float timeBetweenWaves = 20F; // 5F
     private float countdown = 2f;
 
     private int waveNumber = 0;
     private int waveIndex = 20;
 
+    public const int life = 40; // 10
+    public Text LifeCountText;
+    public Text RoundCountText;
+    public Text waveCountdownText;
+
     void Update()
     {
-        if (countdown <= 0f)
+        Enemy = GameObject.FindGameObjectsWithTag("Enemy");
+        enemyCount = Enemy.Length;
+
+        if (countdown <= 0f || enemyCount == 0)
         {
             StartCoroutine(SpawnWave());
             countdown = timeBetweenWaves;
         }
 
         countdown -= Time.deltaTime;
+
+        waveCountdownText.text = Mathf.Round(countdown).ToString();
+
+        LifeCountText.text = (life - enemyCount).ToString();
+
+        if (countdown >= 0f && (life - enemyCount) < 0)
+        {
+            Destroy(gameObject);
+        }
     }
 
   
@@ -29,9 +52,11 @@ public class WaveSpawner : MonoBehaviour
     IEnumerator SpawnWave ()
     {
         waveNumber++;
+        RoundCountText.text = (waveNumber).ToString();
 
         for (int i = 0; i < waveIndex; i++)
         {
+            enemyCount++;
             SpawnEnemy();
             yield return new WaitForSeconds(0.5f);
         }
