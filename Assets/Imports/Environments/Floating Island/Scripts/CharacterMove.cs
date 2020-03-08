268077eab1bda6969483388bb0edb5c978d6e35a
Eliot Jang <37320831+eliotjang@users.noreply.github.com>
2020-03-08 17:13:24 +0900

    add Island landscape (#23)
    * branch eliot
    
    * 2020-03-06
    
    Scene1, mdy island
    
    * remove asset,projectsetting,package
    
    * .
    
    * removed meta files
    
    * initial commit
    
    * removed meta files
    
    * .
    
    * .
    
    * Update .DS_Store
    
    * d
    
    * for upload v1
    
    * init
    
    * initial commit 0306
    
    * Update README.md
    
    * Delete .DS_Store
    
    * Update .gitignore
    
    * Delete .DS_Store
    
    * Update .gitignore
    
    * .
    
    * remove
    
    remove asset packages projectsettings
    
    * add asset, packages, projectsettings
    
    * delete
    
    * fetch from master
    
    * remote bull
    
    * Island landscape
    
    Co-authored-by: SeongHyukJang <seonghyuk578@khu.ac.kr>

---
+++ Assets/Imports/Environments/Floating Island/Scripts/CharacterMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour {
+
+    //This script is for controlling the character gameobject movements.
+
+    public float speed = 10F;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float translation = Input.GetAxis("Vertical") * speed;
+        float straffe= Input.GetAxis("Horizontal") * speed;
+        float translation1 = Input.GetAxis("Jump") * speed;
+
+        translation *= Time.deltaTime;
+        straffe *= Time.deltaTime;
+        translation1 *= Time.deltaTime;
+
+        transform.Translate(straffe, translation1, translation);
+
+        if (Input.GetKeyDown("escape"))
+            Cursor.lockState = CursorLockMode.None;
+
+
+
+    }
+}
