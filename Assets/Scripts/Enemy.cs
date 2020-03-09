966522b8db76f452a7445caefaf2c6a2985818cd
eliotjang <eliotjang2@gmail.com>
2020-03-09 23:31:05 +0900

    2020-03-09
    Add enemy prefab
      - grunt(오크병사)
      - orc_wolf_rider(오크기사[보스1])
      - skeleton archer(스켈레톤 궁수)
      - skeleton mage(스켈레톤 메이지[보스2])
    
    mdy TempScene
      - add enemyRoad
      - add TowerFloor
    
    mdy sky
      - resized cloud
    
    mdy enemy animation
      - smooth walk(오크 및 언데드류)
    
    add Round
      - 1 ~ 10 wave : Orc
      - 11 ~ 20 wave : Undead
    
    mdy WaveSpawner.cs
      - 게임시작전 대기시간: 5 -> 3
      - monster wave time: 5 -> 1

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,7 +6,7 @@ public class Enemy : MonoBehaviour
 {
     //public GameObject tower;
     public int moneyGain = 100;
-    public float speed = 50f;
+    public float speed = 10f;
 
     public int startHealth = 100;
 
