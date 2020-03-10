eda104f0aa37ed5e9d950a792a75e34ab3ae2e09
Eliot Jang <37320831+eliotjang@users.noreply.github.com>
2020-03-10 18:23:28 +0900

    eliot -> v1.0 (#25)
    * branch eliot
    
    * 2020-03-06
    
    Scene1, mdy island
    
    * remove asset,projectsetting,package
    
    * .
    
    * removed meta files
    
    * initial commit
    
    * removed meta files
    
    * .
    
    * .
    
    * Update .DS_Store
    
    * d
    
    * for upload v1
    
    * init
    
    * initial commit 0306
    
    * Update README.md
    
    * Delete .DS_Store
    
    * Update .gitignore
    
    * Delete .DS_Store
    
    * Update .gitignore
    
    * .
    
    * remove
    
    remove asset packages projectsettings
    
    * add asset, packages, projectsettings
    
    * delete
    
    * fetch from master
    
    * remote bull
    
    * Island landscape
    
    * V1.0 ->eliot (#11)
    
    * 2020-03-09
    
    Assets/Scripts/Tower.cs
    한 적에게 모든 스킬을 쓰던것을 랜덤한 적에게 사용하는것으로 변경
    
    * 2020-03-09
    
    TempScene은 밸런스 조절
    TempScene_test는 테스트용
    
    * 2020-03-09
    
    Add enemy prefab
      - grunt(오크병사)
      - orc_wolf_rider(오크기사[보스1])
      - skeleton archer(스켈레톤 궁수)
      - skeleton mage(스켈레톤 메이지[보스2])
    
    mdy TempScene
      - add enemyRoad
      - add TowerFloor
    
    mdy sky
      - resized cloud
    
    mdy enemy animation
      - smooth walk(오크 및 언데드류)
    
    add Round
      - 1 ~ 10 wave : Orc
      - 11 ~ 20 wave : Undead
    
    mdy WaveSpawner.cs
      - 게임시작전 대기시간: 5 -> 3
      - monster wave time: 5 -> 1
    
    Co-authored-by: SeongHyukJang <seonghyuk578@khu.ac.kr>

--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -8,6 +8,7 @@ public class WaveSpawner : MonoBehaviour
     public Transform enemyPrefab1;
     public Transform enemyPrefab1Boss;
     public Transform enemyPrefab2;
+    public Transform enemyPrefab2Boss;
     /*public Transform enemyPrefab3;
     public Transform enemyPrefab4;
     public Transform enemyPrefab5;
@@ -18,11 +19,11 @@ public class WaveSpawner : MonoBehaviour
     //public Transform[] enemyPrefab = { enemy1, enemy2 };
     public Transform spawnPoint;
 
-    public float timeBetweenWaves = 20F; // 5F
-    private float countdown = 2f;
+    private float timeBetweenWaves = 1f; // 20f
+    private float countdown = 3f; // 40f Prepare Skill before Game Start
     
     private int waveNumber = 0;
-    private int waveIndex = 5;
+    private int waveIndex = 2; //20
 
     public Text RoundCountText;
     public Text waveCountdownText;
@@ -34,16 +35,13 @@ public class WaveSpawner : MonoBehaviour
             StartCoroutine(SpawnWave());
             countdown = timeBetweenWaves;
         }
-        else if (LivesUI.enemyCount == 0)
-        {
-            StartCoroutine(SpawnWave());
-            countdown = timeBetweenWaves;
-        }
-
+        //else if (LivesUI.enemyCount == 0)
+        //{
+        //    StartCoroutine(SpawnWave());
+        //    countdown = timeBetweenWaves;
+        //}
         countdown -= Time.deltaTime;
-
         waveCountdownText.text = Mathf.Round(countdown).ToString();
-
     }
     IEnumerator SpawnWave ()
     {
@@ -54,6 +52,10 @@ public class WaveSpawner : MonoBehaviour
         if (waveNumber == 10)
         {
             Instantiate(enemyPrefab1Boss, spawnPoint.position, spawnPoint.rotation);
+        }
+        if (waveNumber == 20)
+        {
+            Instantiate(enemyPrefab2Boss, spawnPoint.position, spawnPoint.rotation);
         }
         for (int i = 0; i < waveIndex; i++)
         {
@@ -68,6 +70,11 @@ public class WaveSpawner : MonoBehaviour
         {
             Instantiate(enemyPrefab1, spawnPoint.position, spawnPoint.rotation);
 
+        }
+
+        if (10 < waveNumber && waveNumber <= 20)
+        {
+            Instantiate(enemyPrefab2, spawnPoint.position, spawnPoint.rotation);
         }
         
         /*switch (waveNumber)
